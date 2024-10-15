46d8ac9856397241c23b47051cc52a02c05f9964
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2024-10-15 10:29:34 +0100

    apply handling of GET requests for a given article id

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -2,7 +2,7 @@ const { request, response } = require("express")
 const { fetchTopics } = require("../models/topics.model")
 const app = require("../app")
 
-exports.getTopics = (request, response) => {
+exports.getTopics = (request, response, next) => {
     fetchTopics()
     .then((topics) => {
         response.status(200).send({topics})
