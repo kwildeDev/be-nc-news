bfff2ae4ceb7676b0363f97a1dd4d17a3cbaa9a2
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2024-10-17 10:37:25 +0100

    facilitate deletion of a comment by comment_id and handle associated errors

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { getArticleById, getArticles, patchArticle } = require("./controllers/articles.controller");
-const { getCommentsByArticleId, postComment } = require("./controllers/comments.controller");
+const { getCommentsByArticleId, postComment, deleteComment } = require("./controllers/comments.controller");
 const endpoints = require("./endpoints.json");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/topics", getTopics);
+// /api
 
 app.get("/api", (request, response) => {
     response.status(200).send({endpoints: endpoints})
 });
 
+// /api/topics
+
+app.get("/api/topics", getTopics);
+
+// /api/articles/:article_id
+
 app.get("/api/articles/:article_id", getArticleById);
 
 app.patch("/api/articles/:article_id", patchArticle);
 
+// /api/articles
+
 app.get("/api/articles", getArticles);
+
+// /api/articles/:article_id/comments
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.post("/api/articles/:article_id/comments", postComment);
+
+// /api/comments/:comment_id
+
+app.delete("/api/comments/:comment_id", deleteComment);
 
 // Error Handling
 
--- models/comments.model.js
+++ models/comments.model.js
@@ -25,3 +25,13 @@ exports.createComment = (article_id, { author, body }) => {
             return result.rows[0];
         });
 }
+
+exports.removeComment = (comment_id) => {
+    return db
+    .query('DELETE FROM comments WHERE comment_id = $1', [comment_id])
+    .then((result) => {
+        if (result.rowCount === 0) {
+            return Promise.reject({ status: 404, msg: "Comment Does Not Exist"})
+        }
+    });
+}
